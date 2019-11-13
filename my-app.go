79e671a3ccd17a978286c6f08a03f8c610f5b4a8
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-11-12 17:56:33 -0800

    Update app (#6)
    * Update app
    
    * Update app
    
    * Update app
    
    * Update app
    
    * Update app

--- my-app.go
+++ my-app.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"time"
 )
 
 var greeting = os.Getenv("GREETING")
 
 func main() {
-	log.Printf("Listening port 8080: %v", greeting)
-	http.HandleFunc("/", handler)
-	panic("TODO")
-	_ = http.ListenAndServe(":8080", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, greeting)
+	for {
+		log.Println(os.Getenv("GREETING"))
+		time.Sleep(5000)
+	}
 }

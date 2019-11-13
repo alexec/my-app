088b2e892eb6387988a097e7e65f8a026a98c66a
Alex Collins <alex_collins@intuit.com>
2019-11-12 18:05:34 -0800

    bad

--- my-app.go
+++ my-app.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"log"
 	"os"
 	"time"
 )
 
 func main() {
 	for {
-		log.Println(os.Getenv("GREETING"))
+		panic(os.Getenv("GREETING"))
 		time.Sleep(1*time.Second)
 	}
 }
